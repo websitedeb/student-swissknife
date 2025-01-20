68326b53954180dd2da58d892c6be18de6568472
paragrghoshal <parag.qapro@gmail.com>
2025-01-19 20:43:42 -0500

    added manifest

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,13 @@ const nextConfig = {
     output: "export",
     images: { 
       unoptimized: true 
-    }
+    },
+    pwa: {
+      dest: 'public',
+      register: true, 
+      skipWaiting: true,
+    },
+    reactStrictMode: true,
   };
   
   export default nextConfig;
